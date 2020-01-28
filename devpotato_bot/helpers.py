4c565168c5c311af8f4d718bcbdba00f6ed86719
cl0ne <cl0ne@mithril.org.ua>
2020-01-29 01:49:23 +0200

    Log message deletion errors

--- devpotato_bot/helpers.py
+++ devpotato_bot/helpers.py
@@ -1,8 +1,11 @@
+import logging
 from functools import wraps
 
 import telegram.error
 from telegram import Update, Message, Bot
 from telegram.ext import CallbackContext
+
+_logger = logging.getLogger(__name__)
 
 
 def deletes_caller_message(func):
@@ -12,7 +15,7 @@ def deletes_caller_message(func):
         message: Message = update.effective_message
         try:
             bot.delete_message(message.chat_id, message.message_id)
-        except telegram.error.BadRequest:
-            pass
+        except telegram.error.BadRequest as e:
+            _logger.warning('Message with id %d was not deleted', exc_info=e)
         return func(update, context)
     return wrapper
