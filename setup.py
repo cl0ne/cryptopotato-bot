97533e2d1f1016ebdce55ba9a80a2a6ea1a4e3ef
cl0ne <cl0ne@mithril.org.ua>
2020-05-15 20:09:35 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='devpotato-bot',
-    version='0.4.2',
+    version='0.5.0',
     description='Telegram bot for cryptopotato chat',
     packages=['devpotato_bot'],
 
