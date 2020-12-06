b2f15383ff7063cac874828f2ef897eaae20f1cb
cl0ne <cl0ne@mithril.org.ua>
2020-12-06 04:29:58 +0200

    Use black to format autogenerated alembic revisions

--- alembic/versions/42ff42f8516a_drop_groupchat_copy_default_titles.py
+++ alembic/versions/42ff42f8516a_drop_groupchat_copy_default_titles.py
@@ -10,17 +10,19 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '42ff42f8516a'
-down_revision = '564bfe5ee644'
+revision = "42ff42f8516a"
+down_revision = "564bfe5ee644"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    with op.batch_alter_table('daily_titles_group_chats', schema=None) as batch_op:
-        batch_op.drop_column('copy_default_titles')
+    with op.batch_alter_table("daily_titles_group_chats", schema=None) as batch_op:
+        batch_op.drop_column("copy_default_titles")
 
 
 def downgrade():
-    with op.batch_alter_table('daily_titles_group_chats', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('copy_default_titles', sa.BOOLEAN(), nullable=False))
+    with op.batch_alter_table("daily_titles_group_chats", schema=None) as batch_op:
+        batch_op.add_column(
+            sa.Column("copy_default_titles", sa.BOOLEAN(), nullable=False)
+        )
