a0fadda88cfce6bebc7e3e964e9c8b3463ff3823
cl0ne <cl0ne@mithril.org.ua>
2020-12-11 01:27:53 +0200

    Update dependecies, bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='devpotato-bot',
-    version='0.5.3',
+    version='0.6.0',
     description='Telegram bot for cryptopotato chat',
     packages=['devpotato_bot'],
 
     python_requires='>=3.8',
     install_requires=[
-        'python-telegram-bot>=12.6',
+        'python-telegram-bot>=13.0',
         'ujson>=3.0.0',
         'cachetools>=4',
         'pytz>=2020.4',
