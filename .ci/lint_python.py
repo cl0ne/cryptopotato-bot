fd09fb208d33b8cdf2b289023a86cd1e28eba905
cl0ne <cl0ne@mithril.org.ua>
2020-12-22 23:54:56 +0200

    Improve pylint wrapper for CI
    Remove full error silencing
    Return zero exit status on SIGINT
    Add script header comment

--- .ci/lint_python.py
+++ .ci/lint_python.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
+# Wrapper script to customize linting of the project with pylint:
+# - provides workaround for https://github.com/PyCQA/pylint/issues/3944 (implicit namespace
+#   packages within regular packages are ignored)
+# - select which message types are considered as critical and contribute to non-zero exit status,
+#   see also http://pylint.pycqa.org/en/latest/user_guide/run.html#exit-codes
+# - explicitly specify linting targets, default rcfile and ignored directory names
 
 import glob
 import itertools
+import functools
 import os
 import os.path
 import sys
 import typing
 
 from pylint.lint import Run as RunLinter
+from pylint.constants import MSG_TYPES_STATUS
 
 
 class DirGlob(typing.NamedTuple):
@@ -31,7 +39,15 @@ MODULE_SEARCH_PATHS = (
 IGNORED_DIRS = ('__pycache__', '.git')
 PYLINT_RCFILE = 'pylintrc'
 
+# Include only specified message types into exit status so builds will fail only
+# when there's critical enough issues found
+# Set this variable to a string of short type names to select message types to be included
+# (see keys from pylint.constants.MSG_TYPES for valid choices)
+# Set to None to disable exit status filtering
+EXIT_STATUS_MASK_STR = 'EF'
+
 PACKAGE_MARKER = '__init__.py'
+WRAPPER_EXIT_FAIL = 32  # same as for usage error in pylint
 
 
 def is_python_file(item: os.DirEntry):
@@ -69,6 +85,17 @@ def find_modules(dirs: typing.Iterable[DirGlob]):
 
 
 def main(args):
+    filter_exit_status = EXIT_STATUS_MASK_STR is not None
+    if filter_exit_status:
+        try:
+            include_statuses = (MSG_TYPES_STATUS[t] for t in EXIT_STATUS_MASK_STR)
+            exit_status_mask = functools.reduce(int.__or__, include_statuses, 0)
+        except KeyError:
+            invalid_types = list(EXIT_STATUS_MASK_STR - MSG_TYPES_STATUS.keys())
+            error_message = 'EXIT_STATUS_MASK_STR contains invalid message type abbreviations:'
+            print(error_message, invalid_types, file=sys.stderr)
+            return WRAPPER_EXIT_FAIL
+
     script_dir = os.path.dirname(os.path.realpath(__file__))
     pylint_config = ['--rcfile', os.path.join(script_dir, PYLINT_RCFILE)]
     linter_args = list(itertools.chain(
@@ -78,11 +105,15 @@ def main(args):
         find_modules(MODULE_SEARCH_PATHS),
         args
     ))
+
     try:
         runner = RunLinter(linter_args, exit=False)
-        return runner.linter.msg_status
+        exit_status = runner.linter.msg_status
+        if filter_exit_status:
+            exit_status &= exit_status_mask
+        return exit_status
     except KeyboardInterrupt:
-        return 64
+        return 0
 
 
 if __name__ == '__main__':
